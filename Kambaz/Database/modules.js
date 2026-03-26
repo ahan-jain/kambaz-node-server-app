5307637ac9a6cdc558c324868f89013df3e9a102
jain-aha <ahan@ahanjain.com>
2026-03-26 17:14:04 -0400

    first commit

---
+++ Kambaz/Database/modules.js
@@ -0,0 +1,266 @@
+export default [
+  {
+    "_id": "M101",
+    "name": "Introduction to Rocket Propulsion",
+    "description": "Basic principles of rocket propulsion and rocket engines.",
+    "course": "RS101",
+    "lessons": [
+      { "_id": "L101", "name": "History of Rocketry", "description": "A brief history of rocketry and space exploration.", "module": "M101" },
+      { "_id": "L102", "name": "Rocket Propulsion Fundamentals", "description": "Basic principles of rocket propulsion.", "module": "M101" },
+      { "_id": "L103", "name": "Rocket Engine Types", "description": "Overview of different types of rocket engines.", "module": "M101" }
+    ]
+  },
+  {
+    "_id": "M102",
+    "name": "Fuel and Combustion",
+    "description": "Understanding rocket fuel, combustion processes, and efficiency.",
+    "course": "RS101",
+    "lessons": [
+      { "_id": "L201", "name": "Rocket Fuel", "description": "Overview of different types of rocket fuels.", "module": "M102" },
+      { "_id": "L202", "name": "Combustion Processes", "description": "Understanding combustion processes and efficiency.", "module": "M102" },
+      { "_id": "L203", "name": "Combustion Instability", "description": "Understanding combustion instability and mitigation.", "module": "M102" }
+    ]
+  },
+  {
+    "_id": "M103",
+    "name": "Nozzle Design",
+    "description": "Principles of rocket nozzle design and performance optimization.",
+    "course": "RS101",
+    "lessons": [
+      { "_id": "L301", "name": "Nozzle Design", "description": "Overview of different types of rocket nozzles.", "module": "M103" },
+      { "_id": "L302", "name": "Nozzle Performance", "description": "Understanding nozzle performance and efficiency.", "module": "M103" },
+      { "_id": "L303", "name": "Nozzle Optimization", "description": "Optimizing nozzle design for specific applications.", "module": "M103" }
+    ]
+  },
+  {
+    "_id": "M201",
+    "name": "Fundamentals of Aerodynamics",
+    "description": "Basic aerodynamic concepts and fluid dynamics principles.",
+    "course": "RS102",
+    "lessons": [
+      { "_id": "L401", "name": "Fluid Dynamics", "description": "Introduction to fluid dynamics.", "module": "M201" },
+      { "_id": "L402", "name": "Bernoulli's Principle", "description": "Understanding Bernoulli's equation.", "module": "M201" },
+      { "_id": "L403", "name": "Airflow Patterns", "description": "Study of airflow patterns.", "module": "M201" }
+    ]
+  },
+  {
+    "_id": "M202",
+    "name": "Subsonic and Supersonic Flow",
+    "description": "Understanding subsonic and supersonic aerodynamic behaviors.",
+    "course": "RS102",
+    "lessons": [
+      { "_id": "L404", "name": "Subsonic Flow", "description": "Basics of subsonic flow.", "module": "M202" },
+      { "_id": "L405", "name": "Supersonic Flow", "description": "Basics of supersonic flow.", "module": "M202" },
+      { "_id": "L406", "name": "Shock Waves", "description": "Understanding shock waves.", "module": "M202" }
+    ]
+  },
+  {
+    "_id": "M203",
+    "name": "Aerodynamic Heating",
+    "description": "Study of aerodynamic heating and thermal protection systems.",
+    "course": "RS102",
+    "lessons": [
+      { "_id": "L407", "name": "Heat Transfer", "description": "Basics of aerodynamic heat transfer.", "module": "M203" },
+      { "_id": "L408", "name": "Thermal Protection", "description": "Thermal protection systems.", "module": "M203" },
+      { "_id": "L409", "name": "Reentry Heating", "description": "Heating during atmospheric reentry.", "module": "M203" }
+    ]
+  },
+  {
+    "_id": "M301",
+    "name": "Spacecraft Structural Design",
+    "description": "Fundamentals of designing spacecraft structures and materials selection.",
+    "course": "RS103",
+    "lessons": [
+      { "_id": "L501", "name": "Structural Loads", "description": "Understanding spacecraft structural loads.", "module": "M301" },
+      { "_id": "L502", "name": "Materials Selection", "description": "Choosing materials for spacecraft.", "module": "M301" },
+      { "_id": "L503", "name": "Structural Testing", "description": "Testing spacecraft structures.", "module": "M301" }
+    ]
+  },
+  {
+    "_id": "M302",
+    "name": "Orbital Mechanics",
+    "description": "Understanding orbital dynamics and mission planning.",
+    "course": "RS103",
+    "lessons": [
+      { "_id": "L504", "name": "Kepler's Laws", "description": "Understanding Kepler's laws of motion.", "module": "M302" },
+      { "_id": "L505", "name": "Orbit Transfers", "description": "Hohmann transfers and orbit changes.", "module": "M302" },
+      { "_id": "L506", "name": "Mission Planning", "description": "Basics of mission planning.", "module": "M302" }
+    ]
+  },
+  {
+    "_id": "M303",
+    "name": "Spacecraft Systems Engineering",
+    "description": "Overview of spacecraft systems and subsystems engineering.",
+    "course": "RS103",
+    "lessons": [
+      { "_id": "L507", "name": "Power Systems", "description": "Spacecraft power systems.", "module": "M303" },
+      { "_id": "L508", "name": "Communication Systems", "description": "Spacecraft communication.", "module": "M303" },
+      { "_id": "L509", "name": "Life Support", "description": "Life support systems.", "module": "M303" }
+    ]
+  },
+  {
+    "_id": "M401",
+    "name": "Organic Compounds",
+    "description": "Structure and properties of organic compounds.",
+    "course": "RS104",
+    "lessons": [
+      { "_id": "L601", "name": "Hydrocarbons", "description": "Study of hydrocarbons.", "module": "M401" },
+      { "_id": "L602", "name": "Functional Groups", "description": "Common functional groups.", "module": "M401" },
+      { "_id": "L603", "name": "Isomers", "description": "Types of isomers.", "module": "M401" }
+    ]
+  },
+  {
+    "_id": "M402",
+    "name": "Organic Reactions",
+    "description": "Common organic reaction mechanisms.",
+    "course": "RS104",
+    "lessons": [
+      { "_id": "L604", "name": "Substitution Reactions", "description": "SN1 and SN2 reactions.", "module": "M402" },
+      { "_id": "L605", "name": "Elimination Reactions", "description": "E1 and E2 reactions.", "module": "M402" },
+      { "_id": "L606", "name": "Addition Reactions", "description": "Addition reaction mechanisms.", "module": "M402" }
+    ]
+  },
+  {
+    "_id": "M403",
+    "name": "Spectroscopy",
+    "description": "Spectroscopic methods for structure determination.",
+    "course": "RS104",
+    "lessons": [
+      { "_id": "L607", "name": "NMR Spectroscopy", "description": "Nuclear magnetic resonance.", "module": "M403" },
+      { "_id": "L608", "name": "IR Spectroscopy", "description": "Infrared spectroscopy.", "module": "M403" },
+      { "_id": "L609", "name": "Mass Spectrometry", "description": "Mass spectrometry basics.", "module": "M403" }
+    ]
+  },
+  {
+    "_id": "M501",
+    "name": "Coordination Chemistry",
+    "description": "Study of coordination compounds.",
+    "course": "RS105",
+    "lessons": [
+      { "_id": "L701", "name": "Ligands", "description": "Types of ligands.", "module": "M501" },
+      { "_id": "L702", "name": "Metal Complexes", "description": "Formation of metal complexes.", "module": "M501" },
+      { "_id": "L703", "name": "Naming Conventions", "description": "IUPAC naming of complexes.", "module": "M501" }
+    ]
+  },
+  {
+    "_id": "M502",
+    "name": "Bonding Theories",
+    "description": "Bonding theories in inorganic chemistry.",
+    "course": "RS105",
+    "lessons": [
+      { "_id": "L704", "name": "Crystal Field Theory", "description": "Basics of crystal field theory.", "module": "M502" },
+      { "_id": "L705", "name": "Molecular Orbital Theory", "description": "MO theory for complexes.", "module": "M502" },
+      { "_id": "L706", "name": "Ligand Field Theory", "description": "Ligand field theory basics.", "module": "M502" }
+    ]
+  },
+  {
+    "_id": "M503",
+    "name": "Bioinorganic Chemistry",
+    "description": "Role of metals in biological systems.",
+    "course": "RS105",
+    "lessons": [
+      { "_id": "L707", "name": "Hemoglobin", "description": "Iron in hemoglobin.", "module": "M503" },
+      { "_id": "L708", "name": "Metalloenzymes", "description": "Metals in enzymes.", "module": "M503" },
+      { "_id": "L709", "name": "Metal Toxicity", "description": "Toxic effects of metals.", "module": "M503" }
+    ]
+  },
+  {
+    "_id": "M601",
+    "name": "Thermodynamics",
+    "description": "Laws of thermodynamics in chemistry.",
+    "course": "RS106",
+    "lessons": [
+      { "_id": "L801", "name": "First Law", "description": "First law of thermodynamics.", "module": "M601" },
+      { "_id": "L802", "name": "Second Law", "description": "Second law and entropy.", "module": "M601" },
+      { "_id": "L803", "name": "Gibbs Free Energy", "description": "Gibbs free energy calculations.", "module": "M601" }
+    ]
+  },
+  {
+    "_id": "M602",
+    "name": "Chemical Kinetics",
+    "description": "Rates of chemical reactions.",
+    "course": "RS106",
+    "lessons": [
+      { "_id": "L804", "name": "Rate Laws", "description": "Determining rate laws.", "module": "M602" },
+      { "_id": "L805", "name": "Reaction Order", "description": "Zero, first, second order reactions.", "module": "M602" },
+      { "_id": "L806", "name": "Catalysis", "description": "Catalysis and reaction rates.", "module": "M602" }
+    ]
+  },
+  {
+    "_id": "M603",
+    "name": "Quantum Chemistry",
+    "description": "Quantum mechanical models in chemistry.",
+    "course": "RS106",
+    "lessons": [
+      { "_id": "L807", "name": "Wave Functions", "description": "Basics of wave functions.", "module": "M603" },
+      { "_id": "L808", "name": "Particle in a Box", "description": "Particle in a box model.", "module": "M603" },
+      { "_id": "L809", "name": "Atomic Orbitals", "description": "Quantum model of atomic orbitals.", "module": "M603" }
+    ]
+  },
+  {
+    "_id": "M701",
+    "name": "Elvish Languages",
+    "description": "Study of Sindarin and Quenya.",
+    "course": "RS107",
+    "lessons": [
+      { "_id": "L901", "name": "Sindarin Basics", "description": "Introduction to Sindarin.", "module": "M701" },
+      { "_id": "L902", "name": "Quenya Basics", "description": "Introduction to Quenya.", "module": "M701" },
+      { "_id": "L903", "name": "Tengwar Script", "description": "Writing in Tengwar.", "module": "M701" }
+    ]
+  },
+  {
+    "_id": "M702",
+    "name": "Dwarvish and Other Scripts",
+    "description": "Study of Khuzdul and other Middle-earth scripts.",
+    "course": "RS107",
+    "lessons": [
+      { "_id": "L904", "name": "Khuzdul Basics", "description": "Introduction to Dwarvish.", "module": "M702" },
+      { "_id": "L905", "name": "Cirth Runes", "description": "The Cirth writing system.", "module": "M702" },
+      { "_id": "L906", "name": "Black Speech", "description": "The language of Mordor.", "module": "M702" }
+    ]
+  },
+  {
+    "_id": "M703",
+    "name": "Comparative Linguistics",
+    "description": "Comparing languages of Middle-earth.",
+    "course": "RS107",
+    "lessons": [
+      { "_id": "L907", "name": "Language Families", "description": "Language families of Middle-earth.", "module": "M703" },
+      { "_id": "L908", "name": "Etymology", "description": "Word origins across languages.", "module": "M703" },
+      { "_id": "L909", "name": "Inscriptions", "description": "Famous inscriptions of Middle-earth.", "module": "M703" }
+    ]
+  },
+  {
+    "_id": "M801",
+    "name": "Historical Alliances",
+    "description": "Key alliances in Middle-earth history.",
+    "course": "RS108",
+    "lessons": [
+      { "_id": "L1001", "name": "Last Alliance", "description": "The Last Alliance of Elves and Men.", "module": "M801" },
+      { "_id": "L1002", "name": "Council of Elrond", "description": "Diplomacy at the Council of Elrond.", "module": "M801" },
+      { "_id": "L1003", "name": "Fellowship Formation", "description": "Formation of the Fellowship.", "module": "M801" }
+    ]
+  },
+  {
+    "_id": "M802",
+    "name": "Inter-species Relations",
+    "description": "Relations between the races of Middle-earth.",
+    "course": "RS108",
+    "lessons": [
+      { "_id": "L1004", "name": "Elf-Dwarf Relations", "description": "History of Elf-Dwarf tensions.", "module": "M802" },
+      { "_id": "L1005", "name": "Men and Elves", "description": "Alliances between Men and Elves.", "module": "M802" },
+      { "_id": "L1006", "name": "Wizards' Role", "description": "The Istari as mediators.", "module": "M802" }
+    ]
+  },
+  {
+    "_id": "M803",
+    "name": "Conflict Resolution",
+    "description": "Diplomacy and conflict resolution in Middle-earth.",
+    "course": "RS108",
+    "lessons": [
+      { "_id": "L1007", "name": "War of the Ring", "description": "Diplomatic failures and the War.", "module": "M803" },
+      { "_id": "L1008", "name": "Peace Treaties", "description": "Post-war peace agreements.", "module": "M803" },
+      { "_id": "L1009", "name": "Leadership and Wisdom", "description": "Role of leadership in peace.", "module": "M803" }
+    ]
+  }
+];
---
+++ Kambaz/Enrollments/routes.js
@@ -0,0 +1,20 @@
+import EnrollmentsDao from "./dao.js";
+export default function EnrollmentRoutes(app, db) {
+  const dao = EnrollmentsDao(db);
+  const enrollUserInCourse = (req, res) => {
+    const currentUser = req.session["currentUser"];
+    if (!currentUser) { res.sendStatus(401); return; }
+    const { courseId } = req.params;
+    dao.enrollUserInCourse(currentUser._id, courseId);
+    res.sendStatus(200);
+  };
+  const unenrollUserFromCourse = (req, res) => {
+    const currentUser = req.session["currentUser"];
+    if (!currentUser) { res.sendStatus(401); return; }
+    const { courseId } = req.params;
+    dao.unenrollUserFromCourse(currentUser._id, courseId);
+    res.sendStatus(200);
+  };
+  app.post("/api/users/current/courses/:courseId/enroll", enrollUserInCourse);
+  app.delete("/api/users/current/courses/:courseId/enroll", unenrollUserFromCourse);
+}
